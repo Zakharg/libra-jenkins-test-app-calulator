32d101d9e3806e28ad395016c99a6ece04dff4d1
George <George@ip-192-168-1-27.ec2.internal>
2018-05-22 13:42:04 -0400

    changes

--- src/main/java/tech/libra/jenkins/test/app/calculator/App.java
+++ src/main/java/tech/libra/jenkins/test/app/calculator/App.java
@@ -1,5 +1,11 @@
 package tech.libra.jenkins.test.app.calculator;
 
-public class App {
+import tech.libra.jenkins.test.calculator.*;
 
+public class App {
+	public static void main(String [] args) {
+		CalculatorImpl calculator = new CalculatorImpl();
+		System.out.println(calculator.sub(10, 5));
+		
+	}
 }
